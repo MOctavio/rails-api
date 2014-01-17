e488b2c2bcfc901d040d6f8c2eda9b621b3801bb
Mario Octavio Jimenez <jimenez303@gmail.com>
2014-01-17 09:55:45 -0600

    skip_before_action :verify_authenticity_token

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  skip_before_action :verify_authenticity_token, :only [:update]
+  skip_before_action :verify_authenticity_token, only: [:update]
 
   before_action :verify_custom_authenticity_token, :only [:update]
 
